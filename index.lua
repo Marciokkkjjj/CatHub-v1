e07075524dabd69cbef4a7f03da063ee3a3de7bc
Marciokkkjjj <148603386+Marciokkkjjj@users.noreply.github.com>
2024-09-10 20:52:25 -0300

    aa

--- index.lua
+++ index.lua
@@ -53,6 +53,47 @@ teleportToClosestPlayer()
 	end    
 })
 
+Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+      		print("button pressed")
+              local Players = game:GetService("Players")
+              local LocalPlayer = Players.LocalPlayer
+              
+              function getClosestPlayer()
+                  local closestPlayer = nil
+                  local shortestDistance = math.huge
+              
+                  for _, player in pairs(Players:GetPlayers()) do
+                      if player ~= LocalPlayer and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
+                          local distance = (LocalPlayer.Character.HumanoidRootPart.Position - player.Character.HumanoidRootPart.Position).magnitude
+                          if distance < shortestDistance then
+                              closestPlayer = player
+                              shortestDistance = distance
+                          end
+                      end
+                  end
+              
+                  return closestPlayer
+              end
+              
+              function teleportToClosestPlayer()
+                  local closestPlayer = getClosestPlayer()
+                  if closestPlayer and closestPlayer.Character and closestPlayer.Character:FindFirstChild("HumanoidRootPart") then
+                      LocalPlayer.Character.HumanoidRootPart.CFrame = closestPlayer.Character.HumanoidRootPart.CFrame
+                  end
+              end
+              
+              -- Chame a função teleportToClosestPlayer quando quiser teleportar
+              teleportToClosestPlayer()
+              
+  	end    
+})
+
+--[[
+Name = <string> - The name of the button.
+Callback = <function> - The function of the button.
+]]
 --[[
 Name = <string> - The name of the toggle.
 Default = <bool> - The default value of the toggle.
