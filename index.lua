f25ca9ee9e97db65f6a2395d48e55364186102a4
Marciokkkjjj <148603386+Marciokkkjjj@users.noreply.github.com>
2024-09-10 20:07:52 -0300

    a

--- index.lua
+++ index.lua
@@ -1,7 +1,9 @@
-echo "# CatHub" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Marciokkkjjj/CatHub.git
-git push -u origin main
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
