7f80b5ad5f84e091f36aa82de7389c4e9d9a6d76
Marciokkkjjj <148603386+Marciokkkjjj@users.noreply.github.com>
2024-09-10 21:02:38 -0300

    aaa

--- index.lua
+++ index.lua
@@ -1,5 +1,5 @@
 print("funcionando")
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/Marciokkkjjj/CatHub-v1/main/Lybrari.lua')))()
 
 local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
